7e7e5dc2d3e64a1070bd35ad75c3181588ecdf0b
NnXNnS <ivanchriztian@gmail.com>
2020-09-03 09:10:58 +0700

    Create assignment.ts

---
+++ assignment.ts
@@ -0,0 +1,18 @@
+let num1:number=10;
+let num2:number=2;
+let res:number=0;
+
+res=num1+num2;
+console.log("Sum:     "+res);
+res=num1-num2;
+console.log("Difference:     "+res);
+res=num1*num2;
+console.log("Product:     "+res);
+res=num1/num2;
+console.log("Quotient:     "+res);
+res=num1%num2;
+console.log("Remainder:     "+res);
+num1++;
+console.log("Value num1 after increment:     "+num1);
+num2--;
+console.log("Value num2 after decrement:     "+num2);
